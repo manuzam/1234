48dbb49f4a81911e1e5b33d2d6e4e85e117eac1b
manuzam <142548975+manuzam@users.noreply.github.com>
2024-09-27 12:06:00 -0300

    .

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log(areaQuadrado(6));
-let media = function(num1,num2,num3)){
+let media = function(num1,num2,num3){
     return (num1+num2+num3)/3;
 }
 console.log(media(num1,num2,num3)    );
