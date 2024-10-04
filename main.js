f818afde71511fb11d114321fe673d49e16a9103
manuzam <142548975+manuzam@users.noreply.github.com>
2024-10-04 11:23:51 -0300

    .

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-let nomeUsuário = 'Laura e Manuela';
-alert('Laura e Manuela')
-function soma (num1, num2){
-return num1+num2;
-}
-console.log(soma(6,4));
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(6));
-let media = function(num1,num2,num3){
-    return (num1+num2+num3)/3;
-}
-parseInt(prompt('Me diga três valores para mim tirar a média'));
-let num1 = parcent(prompt('Qual nº?'));
-let num2 = parcent(prompt('Qual nº?'));
-let num3 = parcent(prompt('Qual nº?'));
-function media (x,y,z){
-    return (x,y,z)/3;
-}
-console.log(media(1,2,3));
-document.write (media(num1, num2,num3));
+// let nomeUsuário = 'Laura e Manuela';
+// alert('Laura e Manuela')
+// function soma (num1, num2){
+// return num1+num2;
+// }
+// console.log(soma(6,4));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(6));
+// let media = function(num1,num2,num3){
+//     return (num1+num2+num3)/3;
+// }
+// parseInt(prompt('Me diga três valores para mim tirar a média'));
+// let num1 = parcent(prompt('Qual nº?'));
+// let num2 = parcent(prompt('Qual nº?'));
+// let num3 = parcent(prompt('Qual nº?'));
+// function media (x,y,z){
+//     return (x,y,z)/3;
+// }
+// console.log(media(1,2,3));
+// document.write (media(num1, num2,num3));
+
