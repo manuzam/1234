57c551d7c8eab1b256e99031251e5907f91dfa5c
manuzam <142548975+manuzam@users.noreply.github.com>
2024-10-04 12:12:24 -0300

    .hjgh

--- main.js
+++ main.js
@@ -20,8 +20,8 @@
 // }
 // console.log(media(1,2,3));
 // document.write (media(num1, num2,num3));
-// let estados=
-// [ 'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Santa Catarina','São Paulo','Sergipe','Tocantins','Rondônia','Roraima'];
+let estados=
+[ 'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Santa Catarina','São Paulo','Sergipe','Tocantins','Rondônia','Roraima'];
 // let i=0;
 // while (i<27){
 //     console.log(i);
@@ -29,7 +29,6 @@
 //     i++;
 // }
 for (let index = 0; index < estados.length; index++) {
-    const element = array[index];
     console.log(index);
     console.log(estados[index]);
 }
